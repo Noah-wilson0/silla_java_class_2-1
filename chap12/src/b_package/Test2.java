98add7f6c913abd42b16bfcaf4a5ca419aeff23b
noah-wilson0 <127368908+noah-wilson0@users.noreply.github.com>
2023-09-12 11:13:39 +0900

    12장 실습
    12장 실습

---
+++ chap12/src/b_package/Test2.java
@@ -0,0 +1,12 @@
+package b_package;
+import a_package.Test1;
+//import a_packge.*;
+public class Test2 {
+    public static void main(String[] args) {
+        Test1 t1=new Test1();
+
+        System.out.println(t1.sum(10,20));
+//        Integer a= 10;
+//        System.out.println(a);
+    }
+}
